ec8620f2092fafe401b381e2c41eef7f0869408c
gcvinuth <g.c.vinuth@gmail.com>
2026-09-10 17:55:37 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,11 +3,11 @@
 // This is the ONLY file you need to edit to make the site work.
 // -----------------------------------------------------------------------
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBN3DrC99ma5nLTVNwCQtCdaYpKigERg_g",
+  authDomain: "quizzy-d6723.firebaseapp.com",
+  databaseURL: "https://quizzy-d6723-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "quizzy-d6723",
+  storageBucket: "quizzy-d6723.firebasestorage.app",
+  messagingSenderId: "271842110431",
+  appId: "1:271842110431:web:a9a8039bd8b78f1c90326a"
 };
